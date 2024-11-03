3fe548b36a2a27218fe58b9eed0099cd1b04cc1a
manoeltech <manoel.programador21@gmail.com>
2024-11-03 16:13:52 -0300

    include validates, associations and methods

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,26 @@
 class Author < ApplicationRecord
+  has_many :books
+  validates :name, presence: true
+  enum status: { active: 0, inactive: 1 }
+  before_create :unique_name, :block_in_status_inactive
+
+
+  private
+
+  def unique_name
+    all_names = Author.pluck(:name)
+    current_author = name
+
+    if all_names.include?(current_author)
+      errors.add(:name, "- Autor #{name} já foi criado!")
+      throw(:abort)
+    end
+  end
+
+  def block_in_status_inactive
+    if self.status == "inactive"
+      errors.add(:status, "- Não crie um autor com status inativo")
+      throw(:abort)
+    end
+  end
 end
